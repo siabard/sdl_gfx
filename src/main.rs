81f94ec43c1514a9068611b73b6b40225153c4c4
yhjang <siabard@gmail.com>
2022-12-19 20:28:30 +0900

    투명효과 처리

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
+use hangul_jaso::*;
 use image::io::Reader as ImageReader;
 use image::DynamicImage;
 use image::GenericImageView;
 use sdl2::event::*;
+use sdl2::gfx::primitives;
 use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
-use sdl2::render::TextureCreator;
+
 use sdl2::render::WindowCanvas;
-use hangul_jaso::*;
-use sdl2::gfx::primitives;
 
 use sdl2::*;
 
@@ -27,7 +27,6 @@ struct KoreanFonts {
     jong: Vec<Vec<u32>>,
 }
 
-
 pub fn build_jaso_bul(t: &dyn ToString) -> (Jaso, Bul) {
     let code = utf8_to_ucs2(t).unwrap();
     let jaso = build_jaso(code).unwrap();
@@ -35,7 +34,6 @@ pub fn build_jaso_bul(t: &dyn ToString) -> (Jaso, Bul) {
 
     (jaso, bul)
 }
-
 
 fn image2hex(img: &DynamicImage, x: u32, y: u32, w: u32, h: u32) -> Vec<u32> {
     let mut rows = vec![];
@@ -65,63 +63,59 @@ fn draw_kor_font(
     c: &char,
     fg: &dyn primitives::ToColor,
     bg: &dyn primitives::ToColor,
-
 ) {
     let texture_creator = canvas.texture_creator();
-    let mut texture = texture_creator.create_texture_target(
-	texture_creator.default_pixel_format(),
-	16,
-	16,
-    ).unwrap();
+    let mut texture = texture_creator
+        .create_texture_target(texture_creator.default_pixel_format(), 16, 16)
+        .unwrap();
 
     let (jaso, bul) = build_jaso_bul(c);
 
     let cho_hex = &font.cho[(jaso.cho + bul.cho.unwrap() * 19) as usize];
     let mid_hex = &font.mid[(jaso.mid + bul.mid.unwrap() * 21) as usize];
     let jong_hex = match bul.jong {
-	Some(jong) => {
-	    &font.jong[(jaso.jong + jong * 28) as usize]
-	}
-	_ => &font.jong[0]
-
+        Some(jong) => &font.jong[(jaso.jong + jong * 28) as usize],
+        _ => &font.jong[0],
     };
-    canvas.with_texture_canvas(&mut texture, |texture_canvas| {
-	texture_canvas.set_blend_mode(render::BlendMode::Blend);
-	texture_canvas.set_draw_color(Color::from(bg.as_rgba()));
-	texture_canvas.clear();
-        for j in 0..16_i16 {
-            let cho = cho_hex[j as usize];
-	    let mid = mid_hex[j as usize];
-	    let jong = jong_hex[j as usize];
-            for i in 0..16_i16 {
-                let vc = (cho << i) & 0x8000;
-		let vm = (mid << i) & 0x8000;
-		let vj = (jong << i) & 0x8000;
+    canvas
+        .with_texture_canvas(&mut texture, |texture_canvas| {
+            texture_canvas.set_blend_mode(render::BlendMode::Blend);
+            texture_canvas.set_draw_color(Color::from(bg.as_rgba()));
+            texture_canvas.clear();
+            for j in 0..16_i16 {
+                let cho = cho_hex[j as usize];
+                let mid = mid_hex[j as usize];
+                let jong = jong_hex[j as usize];
+                for i in 0..16_i16 {
+                    let vc = (cho << i) & 0x8000;
+                    let vm = (mid << i) & 0x8000;
+                    let vj = (jong << i) & 0x8000;
 
-                if vc > 0 {
-                    texture_canvas.pixel(i, j, fg.as_u32()).unwrap();
-                }
-		if vm > 0 {
-                    texture_canvas.pixel(i, j, fg.as_u32()).unwrap();
-                }
-		if vj > 0 {
-                    texture_canvas.pixel(i, j, fg.as_u32()).unwrap();
+                    if vc > 0 {
+                        texture_canvas.pixel(i, j, fg.as_u32()).unwrap();
+                    }
+                    if vm > 0 {
+                        texture_canvas.pixel(i, j, fg.as_u32()).unwrap();
+                    }
+                    if vj > 0 {
+                        texture_canvas.pixel(i, j, fg.as_u32()).unwrap();
+                    }
                 }
             }
-        }
-
-
-    }).unwrap();
-
-    canvas.copy_ex(
-	&texture,
-        Rect::new(0, 0, 16, 16),
-        Rect::new(x, y, 16, 16),
-        0.0,
-        Point::new(0, 0),
-        false,
-        false,
-    ).unwrap();
+        })
+        .unwrap();
+    texture.set_blend_mode(render::BlendMode::Blend);
+    canvas
+        .copy_ex(
+            &texture,
+            Rect::new(0, 0, 16, 16),
+            Rect::new(x, y, 16, 16),
+            0.0,
+            Point::new(0, 0),
+            false,
+            false,
+        )
+        .unwrap();
 }
 
 fn draw_ascii_font(
@@ -135,18 +129,14 @@ fn draw_ascii_font(
 ) {
     let texture_creator = canvas.texture_creator();
     let mut texture = texture_creator
-        .create_texture_target(
-            texture_creator.default_pixel_format(),
-            8,
-            16,
-        )
+        .create_texture_target(texture_creator.default_pixel_format(), 8, 16)
         .unwrap();
 
     canvas
         .with_texture_canvas(&mut texture, |texture_canvas| {
-	    texture_canvas.set_blend_mode(render::BlendMode::Blend);
-	    texture_canvas.set_draw_color(Color::from(bg.as_rgba()));
-	    texture_canvas.clear();
+            texture_canvas.set_blend_mode(render::BlendMode::Blend);
+            texture_canvas.set_draw_color(Color::from(bg.as_rgba()));
+            texture_canvas.clear();
 
             for j in 0..16_i16 {
                 let row = font.fonts[*contents as usize][j as usize];
@@ -154,23 +144,24 @@ fn draw_ascii_font(
                     let v = (row << i) & 0x80;
                     if v > 0 {
                         texture_canvas.pixel(i, j, fg.as_u32()).unwrap();
-		    }
+                    }
                 }
             }
-
-
-
         })
         .unwrap();
-    canvas.copy_ex(
-        &texture,
-        Rect::new(0, 0, 8, 16),
-        Rect::new(x, y, 8, 16),
-        0.0,
-        Point::new(0, 0),
-        false,
-        false,
-    ).unwrap();
+
+    texture.set_blend_mode(render::BlendMode::Blend);
+    canvas
+        .copy_ex(
+            &texture,
+            Rect::new(0, 0, 8, 16),
+            Rect::new(x, y, 8, 16),
+            0.0,
+            Point::new(0, 0),
+            false,
+            false,
+        )
+        .unwrap();
 }
 
 fn main() -> Result<(), String> {
@@ -189,7 +180,10 @@ fn main() -> Result<(), String> {
         .decode()
         .unwrap();
 
-    let han_img_font = ImageReader::open("assets/bitmap_fonts/hangul-dkby-dinaru-2.png").unwrap().decode().unwrap();
+    let han_img_font = ImageReader::open("assets/bitmap_fonts/hangul-dkby-dinaru-2.png")
+        .unwrap()
+        .decode()
+        .unwrap();
 
     // 영문 가로 16글자, 세로 8글자, 각 글자는 8x16
     for y in 0..8 {
@@ -202,24 +196,24 @@ fn main() -> Result<(), String> {
     // 한글 가로 28글자, 세로 16글자(8,4,4), 각 글자는 16x16
     // 한글 초성 8벌 : 19 : 32*19*8 = 4864
     for y in 0..8 {
-	for x in 0..19 {
-	    let rows = image2hex(&han_img_font, x * 16, y * 16, 16, 16);
-	    kor_font.cho.push(rows);
-	}
+        for x in 0..19 {
+            let rows = image2hex(&han_img_font, x * 16, y * 16, 16, 16);
+            kor_font.cho.push(rows);
+        }
     }
     // 한글 중성 4벌 : 21 : 32*21*4 = 2688
     for y in 8..12 {
-	for x in 0..21 {
-	    let rows = image2hex(&han_img_font, x * 16, y * 16, 16, 16);
-	    kor_font.mid.push(rows);
-	}
+        for x in 0..21 {
+            let rows = image2hex(&han_img_font, x * 16, y * 16, 16, 16);
+            kor_font.mid.push(rows);
+        }
     }
     // 한글 종성 4벌 : 28 : 32*28*4 = 3584
     for y in 12..16 {
-	for x in 0..28 {
-	    let rows = image2hex(&han_img_font, x * 16, y * 16, 16, 16);
-	    kor_font.jong.push(rows);
-	}
+        for x in 0..28 {
+            let rows = image2hex(&han_img_font, x * 16, y * 16, 16, 16);
+            kor_font.jong.push(rows);
+        }
     }
 
     'running: loop {
@@ -232,36 +226,47 @@ fn main() -> Result<(), String> {
             }
         }
 
-	canvas.set_blend_mode(render::BlendMode::Blend);
-        canvas.set_draw_color(pixels::Color::RGB(127, 127, 127));
+        canvas.set_blend_mode(render::BlendMode::Blend);
+        canvas.set_draw_color(pixels::Color::RGBA(127, 127, 127, 255));
         canvas.clear();
 
-	let text = "This text. 다람쥐쳇바퀴돌리고파";
-	let mut x_target = 100;
-	let y_target = 100;
+        let text = "This text. 다람쥐쳇바퀴돌리고파";
+        let mut x_target = 100;
+        let y_target = 100;
 
-	for c in text.to_string().chars() {
-	    let code = utf8_to_ucs2(&c).unwrap();
+        for c in text.to_string().chars() {
+            let code = utf8_to_ucs2(&c).unwrap();
             let lang = ucs2_language(code);
 
+            match lang {
+                Languages::Ascii => {
+                    draw_ascii_font(
+                        &eng_font,
+                        &mut canvas,
+                        x_target,
+                        y_target,
+                        &c,
+                        &(255, 0, 0, 255),
+                        &(0, 0, 0, 0),
+                    );
+                    x_target += 8;
+                }
+                Languages::Hangul => {
+                    draw_kor_font(
+                        &kor_font,
+                        &mut canvas,
+                        x_target,
+                        y_target,
+                        &c,
+                        &(0, 255, 0, 255),
+                        &(0, 0, 0, 0),
+                    );
+                    x_target += 16;
+                }
 
-	    match lang {
-		Languages::Ascii => {
-		    draw_ascii_font(&eng_font, &mut canvas, x_target, y_target, &c, &(255, 0, 0, 255), &(0, 0, 0, 0));
-		    x_target += 8;
-		},
-		Languages::Hangul => {
-		    draw_kor_font(&kor_font, &mut canvas, x_target, y_target, &c, &(0, 255, 0, 255), &(0, 0, 0, 0));
-		    x_target += 16;
-		},
-
-		
-		_ => {}
-	    }
-
-	}
-
-
+                _ => {}
+            }
+        }
 
         canvas.present();
 
